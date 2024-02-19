c7d1c0156bc5878a823e8466701a667cd113e495
Julien Caucheteux <jdaodkql@gmail.com>
2024-02-19 14:19:43 +0100

    config: add eslint and prettier to the backend

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,13 @@
-const express = require('express')
-const PORT = 3000
+const express = require('express');
+const PORT = 3000;
 
-const app = express()
-app.use(express.json())
+const app = express();
+app.use(express.json());
 
 app.listen(PORT, () => {
-	console.log(`Server Started at ${3000}`)
-})
+  console.log(`Server Started at ${3000}`);
+});
 
 app.get('/', (req, res) => {
-	res.send('Hello World!')
-})
+  res.send('Hello World!');
+});
